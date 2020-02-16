ee20dfc59f879eb8216b716b1faee7ddd1b84557
iMac 27 <andrewjamesadams@live.com>
2020-02-16 16:33:17 -0600

    twenty1 and stream updates.

---
+++ Streaming/thanks.js
@@ -0,0 +1,16 @@
+var peeps = [
+    '',
+    'Thanks for watching',
+    'BrickinNick',
+    'BOB',
+    'TEXAS',
+    'COSPLAY',
+    'DRUGS',
+    'Sgt. McGruff',
+    'Waldo',
+    'Steve',
+    'Ted',
+    'Brian',
+    'Harold',
+    'Myself'
+    ];
