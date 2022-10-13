d9118902ec3869783f18c0f666451cb6020ca30d
AndyJamesAdams <andrewjamesadams@live.com>
2022-10-12 21:27:57 -0500

    Dasher update- Basic unit movement programmed TODO: Stop feedback Walls Boxes Weak Spaces Start and end points Enemies Traps All the art lol All the sound lol

--- Dasher/js/game.js
+++ Dasher/js/game.js
@@ -23,25 +23,63 @@ function animate(){
   window.requestAnimationFrame(animate);
   ctx.clearRect(0,0,innerWidth,innerHeight);
   grid.draw(ctx);
-  unit.update();
+  dir = unit.update(dir);
 
   //// DEBUG:
   ctx.fillText("Dir:"+dir,20,20);
+
 }
   //*************************************************
   //*************************************************
+
+function scanGrid(startR,startC,dir){
+  let end = {r:startR, c:startC};
+  if(dir == 1){ //UP aka decrease R
+    for(let r = startR; r > -1; r--){
+      //Check for blocks/walls/enemies/whatev here
+      end.r = r;
+    }
+  } else if(dir == 3){ //DN aka increase req
+    for(let r = startR; r < grid.rows; r++){
+      end.r = r;
+    }
+  } else if(dir == 2){
+    for(let c = startC; c < grid.columns; c++){
+      end.c = c;
+    }
+  } else if(dir == 4){
+    for(let c = startC; c > -1; c--){
+      end.c = c;
+    }
+  }
+  return end;
+}
 
 //*** INPUT SECTION **//
   window.addEventListener('keyup',function(e){
       console.log(e.key);
       if(dir == 0){
         switch(e.key){
-          case 'ArrowUp': dir = 1; break;
-          case 'ArrowDown': dir = 3; break;
-          case 'ArrowLeft': dir = 4; break;
-          case 'ArrowRight': dir = 2; break;
-          default: dir = 0; break;
+          case 'ArrowUp':
+            dir = 1;
+            break;
+          case 'ArrowDown':
+            dir = 3;
+            break;
+          case 'ArrowLeft':
+            dir = 4;
+            break;
+          case 'ArrowRight':
+            dir = 2;
+            break;
+          default:
+            dir = 0;
+            break;
         }
+        let target = scanGrid(unit.r, unit.c, dir);
+        let targPos = grid.pos[target.r][target.c];
+        console.log(target.r + "," +target.c + " / " + targPos.x + " : "+targPos.y);
+        unit.target(target.r, target.c,targPos);
       }
   });
 
--- Dasher/js/unit.js
+++ Dasher/js/unit.js
@@ -5,30 +5,43 @@ function Unit(r,c,pos, scale){
   this.targX = this.x; this.targY = this.y;
   this.scale = scale;
   this.inPosition = {x: true, y: true};
+  this.moveDampner = 5;
 
   this.draw = function(){
     ctx.fillRect(this.x-this.scale/2,this.y-this.scale/2,this.scale,this.scale);
+    ctx.fillText((this.x-this.targX)+","+(this.y-this.targY),this.x,this.y);
   }
 
-  this.update = function(){
+  this.update = function(dir){
     this.inPosition.x = this.inPosition.y = false;
-    if(this.x < this.targX){
-      //Send left
-    } else if(this.x > this.targX){
+    if(this.x < this.targX && Math.abs(this.x-this.targX) > 1){
       //Send right
+      this.x += (this.targX-this.x)/this.moveDampner;
+    } else if(this.x > this.targX && Math.abs(this.x-this.targX) > 1){
+      //Send left
+      this.x -= (this.x-this.targX)/this.moveDampner;
     } else {
       this.inPosition.x = true;
       this.c = this.targC;
+      this.targX = this.x;
     }
-    if(this.y < this.targY){
+    if(this.y < this.targY && Math.abs(this.y-this.targY) > 1){
       //Send down
-    } else if(this.y > this.targY){
+      this.y += (this.targY-this.y)/this.moveDampner;
+    } else if(this.y > this.targY  && Math.abs(this.y-this.targY) > 1){
       //Send up
+      this.y -= (this.y-this.targY)/this.moveDampner;
     } else {
       this.inPosition.y = true;
       this.r = this.targR;
+      this.targY = this.y;
     }
     this.draw();
+    if(this.ready()){
+      return 0;
+    } else {
+      return dir;
+    }
   }
 
   this.target = function(r,c,pos){
@@ -36,5 +49,7 @@ function Unit(r,c,pos, scale){
     this.targX = pos.x; this.targY = pos.y;
   }
 
-
+  this.ready = function(){
+    return this.inPosition.x && this.inPosition.y;
+  }
 }
